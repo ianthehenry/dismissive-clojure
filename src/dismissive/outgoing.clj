1b24a51337f44e8ab507ea873226b188ea65cac3
Ian Henry <ianthehenry@gmail.com>
2014-07-13 17:59:49 -0400

    only mark a message as sent if no error occurs

--- src/dismissive/outgoing.clj
+++ src/dismissive/outgoing.clj
@@ -9,6 +9,7 @@
             [dismissive.config :refer [mandrill-key]]
             [dismissive.db :refer [incomplete-messages-before complete-message]]))
 
+; blocking. returns true if the send succeeds; false if it fails.
 (defn send-email [to subject body reply-to]
   (let [message {:auto_html false
                  :to [{:email to}]
@@ -16,10 +17,14 @@
                  :from_email "somethinginteresting@example.com"
                  :headers {"Reply-To" reply-to}
                  :subject subject
-                 :text body}]
-    (http/post "https://mandrillapp.com/api/1.0/messages/send"
-               {:body (json/write-str {:message message
-                                       :key mandrill-key})})))
+                 :text body}
+        opts {:body (json/write-str {:message message
+                                     :key mandrill-key})}
+        result @(http/post "https://mandrillapp.com/api/1.0/messages/send" opts)]
+    (if-let [error (result :error)]
+      (do (println "An error occurred:" error)
+        false)
+      (= (result :status) 200))))
 
 (defonce outgoing-mail-ch (chan))
 
@@ -29,11 +34,11 @@
 
 (go-loop []
          (let [message (<! outgoing-mail-ch)]
-           @(send-email "ianthehenry@gmail.com"
-                        (:subject message)
-                        (:body message)
-                        "noreply@example.org")
-           (complete-message message)
+           (when (send-email "ianthehenry@gmail.com"
+                             (:subject message)
+                             (:body message)
+                             "noreply@example.org")
+             (complete-message message))
            (recur)))
 
 (defn new-scheduler []
